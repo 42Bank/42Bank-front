ac0a1b6bc2c40ef6fb2c9bad637598f5eb8d4a8a
jiyoonhur <jiyoon1156@gmail.com>
2020-12-15 12:23:27 +0900

    ADD: shop done

--- front/src/pages/Shop.js
+++ front/src/pages/Shop.js
@@ -1,15 +1,20 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styled, { createGlobalStyle } from 'styled-components';
 import TopBar from '../components/TopBar';
 import NavBar from '../components/NavBar';
+import Sticker from '../imgs/products/sticker.jpeg';
 
 const Shop = () => (
 	<>
 		<Total/>
 		<TopBar>42 월렛 상점</TopBar>
 		<StyledShop>
-			<StyledProducts>1</StyledProducts>
-			<StyledProducts>2</StyledProducts>
+			<ShopList>
+				<StyledImg></StyledImg>
+				<StyledTxt>42 Sticker<br/>2 ₳</StyledTxt>
+			</ShopList>
+			<StyledProducts></StyledProducts>
 			<StyledProducts>3</StyledProducts>
 			<StyledProducts>4</StyledProducts>
 			<StyledProducts>5</StyledProducts>
@@ -17,8 +22,8 @@ const Shop = () => (
 			<StyledProducts>7</StyledProducts>
 			<StyledProducts>8</StyledProducts>
 			<StyledProducts>9</StyledProducts>
-			<NavBar/>
 		</StyledShop>
+		<NavBar/>
 	</>
 );
 
@@ -26,26 +31,52 @@ const Total = createGlobalStyle`
 body {
 	background-color: #58B7BA;
 	margin: 0;
+	box-sizing: border-box;
 }
 `
 
-const StyledShop = styled.div`
+const StyledShop = styled.ul`
 	display: flex;
 	flex-wrap: wrap;
 	flex-direction: row;
-	flex-basis: 50%;
-	background: #58B7BA;
-	padding-top: 2rem;
-	padding-bottom: 10rem;
 	justify-content: space-between;
+	width: 100%;
+	margin: 0;
+	padding: 2rem 3rem 10rem 3rem;
+	box-sizing: border-box;
 `
 
-const StyledProducts = styled.div`
-	width: 40%;
+const ShopList = ( {children} ) => (
+	<StyledProducts>{children}</StyledProducts>
+);
+
+ShopList.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const StyledProducts = styled.li`
+	width: 45%;
 	height: 15rem;
 	background: #fff;
 	border-radius: 20px;
-	margin: 1rem 3rem;
+	margin: 2rem 0rem;
+	list-style: none;
+`
+
+const StyledImg = styled.div`
+	width: 100%;
+	height: 60%;
+	border-radius: 20px 20px 0 0;
+	background-position: center;
+	background-size: cover;
+	background-image: url(${Sticker});
+`
+
+const StyledTxt = styled.div`
+	text-align: center;
+	font-size: 20px;
+	line-height: 30px;
+	padding: 1rem;
 `
 
 export default Shop
